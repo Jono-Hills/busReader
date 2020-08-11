80081d624abdb22d1549a1b290a2e8b77def2b40
Jono-Hills <jonathan.hills@trineo.co.nz>
2020-08-11 20:19:53 +1200

    first pass

---
+++ main.rb
@@ -0,0 +1,81 @@
+# Roadmap
+#
+# Use net:http for request
+# Args
+#     Bus, Stop (number or name)
+require 'nokogiri'
+require 'net/http'
+
+class BusStopReader
+
+  def initialize
+    @bus = "17"
+    @stop = "654"
+    @returnAll = true
+  end
+
+  def printNextBus
+    puts "\nNext Bus:\n+++++++++"
+    uri = URI('https://www.metroinfo.co.nz/api/nextbus')
+    uri.query = URI.encode_www_form(:PlatformTag => @stop)
+
+    responseRaw = Net::HTTP.get(uri)
+    response = Nokogiri::XML(responseRaw).remove_namespaces!
+    routes = response.xpath("//Route")
+
+    routes.each do |route|
+      busNo = route.attributes["RouteNo"].value
+
+      if (@returnAll || busNo == @bus)
+
+        puts busNo + "|\n---"
+        trips = route.children[1].children
+        i = 1
+
+        while i < trips.size()
+          puts "     #{trips[i].attributes["ETA"].value}m"
+          i += 2
+        end
+
+        puts "========"
+        break unless @returnAll
+      end
+    end
+  end
+
+  def parseArgs
+    for i in 0...ARGV.length
+      arg = ARGV[i].split('=')
+
+      case arg[0]
+      when "--bus"
+        @bus = getArgValue(arg)
+        @returnAll = false
+
+      when "--stop"
+        @stop = getArgValue(arg)
+
+      else
+        raise ArgumentError.new(arg[0])
+      end
+    end
+  end
+
+  def getArgValue(arg)
+    raise ArgumentError.new(arg[0]) if arg.length != 2
+    arg[1]
+  end
+end
+
+class ArgumentError < StandardError
+    def initialize(arg)
+      msg = "Invalid argument usage: " + arg + ". Available arguments: --bus=<busNumber>, --stop=<stopNumber>"
+      super(msg)
+    end
+end
+
+
+busReader = BusStopReader.new
+busReader.parseArgs
+busReader.printNextBus
+
